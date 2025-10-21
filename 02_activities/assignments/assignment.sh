78d0e56a4f89dd41804f93f13779e23a65262be0
Ali Yassine <alimhdyassine@gmail.com>
2025-10-21 16:53:32 -0400

    fix: Use rm -f to prevent script error on missing files

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -x
 
 ############################################
 # DSI CONSULTING INC. Project setup script #
@@ -13,10 +13,10 @@ if [ -d newproject ]; then
   echo "Directory 'newproject' already exists. Please remove it before running this script."
   exit 1
 fi
-mkdir -p newproject
+mkdir newproject
 cd newproject
 
-mkdir -p analysis output
+mkdir analysis output
 touch README.md
 touch analysis/main.py
 
@@ -27,42 +27,30 @@ unzip -q rawdata.zip
 ###########################################
 # Complete assignment here
 
-
 # 1. Create a directory named data
 mkdir data
-
 # 2. Move the ./rawdata directory to ./data/raw
 mv ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
-cd data/raw
-ls
+ls ./data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-cd ..
-mkdir processed
-cd processed
-mkdir server_logs
-mkdir user_logs
-mkdir event_logs
+mkdir -p ./data/processed/server_logs
+mkdir -p ./data/processed/user_logs
+mkdir -p ./data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cd..
-cp raw/*server*.log processed/server_logs
-# 6. Repeat the above step for user logs and event logs
-cp raw/*user*.log processed/user_logs
-cp raw/*event*.log processed/event_logs
-# 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-cd raw
-rm *ipaddr*
-cd ..
-cd processed/user_logs
-rm *ipaddr*
-# 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-cd ..
-cd ..
-ls processed/event_logs > inventory.txt
-ls processed/server_logs >> inventory.txt
-ls processed/user_logs >> inventory.txt
+cp ./data/raw/*server*.log ./data/processed/server_logs/
 
-###########################################
+# 6. Repeat the above step for user logs and event logs
+cp ./data/raw/*user*.log ./data/processed/user_logs/
+cp ./data/raw/*event*.log ./data/processed/event_logs/
+
+# 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+# Use the -f flag to prevent an error if no files match the pattern.
+rm -f ./data/raw/*ipaddr*.log
+rm -f ./data/processed/user_logs/*ipaddr*.log
+
+# 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+find ./data/processed -type f > ./data/inventory.txt
 
 ###########################################
 
